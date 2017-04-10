4f3eebe3e4f317e7240c0e863d09a42e23153641
sashapush <sashapush@tut.by>
2017-04-10 19:49:33 +0300

    Added new methods in scope of the lesson

--- sandbox/src/main/java/by/mmjava/sandbox/Fuck.java
+++ sandbox/src/main/java/by/mmjava/sandbox/Fuck.java
@@ -2,6 +2,28 @@ package by.mmjava.sandbox;
 
 public class Fuck {
 	public static void main(String[]  args) {
-	System.out.println ("Hell World!");
-	}
+		double l=78;
+		double s=l*l;
+	hello("Dick Grayson");
+        hello("Chelik");
+        hello("Emptyness");
+
+		//System.out.println("something " + l + " other thing " + s);
+        double azaza =5;
+        System.out.println("Area of square with " +azaza+ " side"+ " is = "  + area(azaza) + " metrov");
+        double a=4;
+        double b=44;
+        System.out.println("Area of pentagon= " + area(a,b)+" metrov");
+    }
+	public static void hello(String smb){
+
+        System.out.println ("Hell MR "+ smb + "!");
+    }
+    public static double area (double len){
+		    return len*len;
+
+    }
+    public static double area (double a,double b){
+        return a*b;
+    }
 }
