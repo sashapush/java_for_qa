4117aed66a720091e9b74c11e1a2ac2711190684
sashapush <sashapush@tut.by>
2017-04-14 22:10:13 +0300

    Implemented call from method

--- sandbox/src/main/java/by/mmjava/sandbox/Point.java
+++ sandbox/src/main/java/by/mmjava/sandbox/Point.java
@@ -6,17 +6,13 @@ package by.mmjava.sandbox;
 public class Point {
     public double x;
     public double y;
-    public double x1;
-    public double y1;
 
-    public Point(double x, double y,double x1,double y1) {
+
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
-        this.y1 = y1;
-        this.x1 = x1;
+
     }
-    public  double distancemethod() {
-        return Math.sqrt(Math.pow((this.x-this.x1), 2) + Math.pow((this.y-this.y1), 2));
     }
 
 }
--- sandbox/src/main/java/by/mmjava/sandbox/PointRun.java
+++ sandbox/src/main/java/by/mmjava/sandbox/PointRun.java
@@ -5,13 +5,17 @@ package by.mmjava.sandbox;
  */
 public class PointRun {
     public static void main(String[] args) {
-        //Point p1 = new Point(100, 2);
-        //Point p2 = new Point(4, 33);
-        Point p3 = new Point(100,2,4,33);
-           System.out.println("Path between [" + p3.x + ";"+ p3.y + "] and [" +p3.x1+";"+p3.y1 + "] is = " + p3.distancemethod());
+        Point p1 = new Point(100, 2);
+        Point p2 = new Point(4, 33);
+        System.out.println("Path between [" + p1.x + ";"+ p1.y + "] and [" +p2.x+";"+p2.y + "] is = " + p1.distance(p2));
+
+
+        System.out.println("Path between [" + p1.x + ";"+ p1.y + "] and [" +p2.x+";"+p2.y + "] is = " + distance(p1,p2));
+
     }
-    /*public static double distance(Point p1, Point p2) {
+    public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow((p1.x-p2.x), 2) + Math.pow((p1.y-p2.y), 2));
-    }*/
+
+    }
 
 }
