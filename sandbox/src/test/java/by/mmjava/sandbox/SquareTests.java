2c523d7efc29f5dacb0b1acae73779eea3c8af47
sashapush <sashapush@tut.by>
2017-04-15 18:16:36 +0300

    Created tests for Square class

---
+++ sandbox/src/test/java/by/mmjava/sandbox/SquareTests.java
@@ -0,0 +1,15 @@
+package by.mmjava.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by User on 4/15/2017.
+ */
+public class SquareTests {
+    @Test     //аннотация
+    public void testArea() {
+Square s = new Square(5);
+        Assert.assertEquals(s.area(),25.0);
+    }
+}
