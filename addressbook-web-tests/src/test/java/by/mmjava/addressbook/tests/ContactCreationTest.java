84c8d57b310335b64b8973c508416ba21c64101f
sashapush <sashapush@tut.by>
2017-04-24 00:24:39 +0300

    testContactCreation is calling applicatiomanager

--- addressbook-web-tests/src/test/java/by/mmjava/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/by/mmjava/addressbook/tests/ContactCreationTest.java
@@ -9,12 +9,11 @@ public class ContactCreationTest extends TestBase{
 
     @Test
     public void testContactCreation() {
-        gotoAddNewContact();
+        app.getNavigationHelper().gotoAddNewContact();
         //to do - send photo
-        addContactData(new ContactData("alex", "middle", "last", "nick", "title", "company name", "address 1", "12345", "67894", "54564", "11", "tests@tests.com", "test2@tests.com", "test3@tests.com", "http://tests.com", "address 1\naddress 2", "1992", "2222", "secondary address", "secondary home", "secondary notes"));
-        submitContactData();
-        viewCreatedContactData();
-        wd.findElement(By.xpath("//tr[@class='odd']/td[7]/a/img")).click();
-    }
+        app.getContactHelper().addContactData(new ContactData("alex", "middle", "last", "nick", "title", "company name", "address 1", "12345", "67894", "54564", "11", "tests@tests.com", "test2@tests.com", "test3@tests.com", "http://tests.com", "address 1\naddress 2", "1992", "2222", "secondary address", "secondary home", "secondary notes"));
+        app.getContactHelper().submitContactData();
+        app.getContactHelper().viewCreatedContactData();
+        }
 
 }
