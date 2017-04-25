1734bbb5a8028e7910f4530249697f5ce8ea6e42
sashapush <sashapush@tut.by>
2017-04-25 18:04:53 +0300

    check GroupCreationTest with "null" values

--- addressbook-web-tests/src/test/java/by/mmjava/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/by/mmjava/addressbook/appmanager/HelperBase.java
@@ -22,9 +22,10 @@ public class HelperBase {
     protected void type(By locator, String text) {
         click(By.cssSelector("body"));
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
-    }
+        if (text!=null){
+            wd.findElement(locator).clear();
+            wd.findElement(locator).sendKeys(text);
+        } }
 
     public  boolean isAlertPresent() {
         try {
