874cc8c282be632a170f9a8440724355bfa0e5d4
sashapush <sashapush@tut.by>
2017-05-05 21:29:08 +0300

    Added list support for GroupCreation and Id of group + comparing Lists of Group

--- addressbook-web-tests/src/test/java/by/mmjava/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/mmjava/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import by.mmjava.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -11,11 +12,21 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         //for (int i=0;i<15;i++) {
-            app.getNavigationHelper().gotoGroupsPage();
-            List<GroupData> before = app.getGroupHelper().getGroupList();
-            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-            List<GroupData> after = app.getGroupHelper().getGroupList();
-            Assert.assertEquals(after.size(), before.size() + 1);
+        app.getNavigationHelper().gotoGroupsPage();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
         //}
+        int max=0;
+        for (GroupData g : after){
+            if (g.getId()>max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
     }
 }
